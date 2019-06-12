90a4967c181aa6ee0c316045920e1696a21bc4d0
sajad1993gh <sajad1993gh@gmail.com>
2019-06-12 19:21:50 -0400

    Firebase set up

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+      apiKey: "AIzaSyCjAWn70J613gtrdw7qA1hXQNBEIrzt7ZQ",
+      authDomain: "train-scheduler-8e607.firebaseapp.com",
+      databaseURL: "https://train-scheduler-8e607.firebaseio.com",
+      projectId: "train-scheduler-8e607",
+      storageBucket: "",
+      messagingSenderId: "87112072927",
+      appId: "1:87112072927:web:0c2382f96b720c9d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
